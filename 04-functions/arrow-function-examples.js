b9a3c8fe15e854d2b83a1c24b391832daf8441ec
John Hunt <john@matraining.com>
2019-11-27 12:49:14 +0000

    Reordered examples

--- 04-functions/arrow-function-examples.js
+++ 04-functions/arrow-function-examples.js
@@ -7,6 +7,13 @@ var func = () => {
 
 func();
 
+var func2 = () => console.log('Hello World');
+
+// Arrow functions return a value by defualt
+var hello = () => "Hello World!'";
+console.log('hello():', hello());
+
+// Arrow fucntions can take parameters
 var printMessage = (message) => {
     console.log(message);
 }
@@ -15,11 +22,7 @@ printMessage('Hello JavaScript world');
 // Short hand form
 printMessage = message => console.log(message);
 
-
 printMessage('Hello Short Hand world');
 
-// Arrow functions return a value by defualt
-var hello = () => "Hello World!'";
-console.log('hello():', hello());
 
 
