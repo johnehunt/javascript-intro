bdb810de62813d45951844dbf6a8e53597cc63c9
John Hunt <john.hunt10@gmail.com>
2022-10-04 12:30:58 +0100

    Updated to use new Jasmine

---
+++ 13-testing/testing/simple-test-bad.js
@@ -0,0 +1,6 @@
+
+  describe('simple calculator test', () => {
+    it('1 + 1 should equal 2', () => {
+      expect(1 + 1).toEqual(1);
+    });
+  });
---
+++ 13-testing/testing/test-myscript1.js
@@ -0,0 +1,22 @@
+describe("Tesing the functions in myscript", function() {
+    describe("Testing divideBy(x, y) function", function() {
+      it("should return the result (2) of dividing 4 by 2", function() {
+        expect(divideBy(4, 2)).toBe(2);
+      });
+      it("should return the result (1.33333) of dividing 4 by 3", function() {
+        expect(divideBy(4, 3)).toBe(1.3333333333333333);
+      });
+      it("should return the result (POSITIVE_INFINITY) of dividing 4 by 0", function() {
+        expect(divideBy(4, 0)).toBe(Number.POSITIVE_INFINITY);
+      });
+    });
+  
+    describe("testing the isNumeric(num) function", function() {
+      it('should return true for "4"', function() {
+        expect(isNumeric("4")).toBe(true);
+      });
+      it('should return false for "A"', function() {
+        expect(isNumeric("A")).toBe(false);
+      });
+    });
+  });
